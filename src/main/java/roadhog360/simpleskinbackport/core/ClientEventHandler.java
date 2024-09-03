066f1a0e859de54166afd0cc5c36c6adb3696b4a
roadhog360 <roadhog360@outlook.com>
2024-09-02 21:55:23 -0500

    Fix #2
    Signed-off-by: roadhog360 <roadhog360@outlook.com>

--- src/main/java/roadhog360/simpleskinbackport/core/ClientEventHandler.java
+++ src/main/java/roadhog360/simpleskinbackport/core/ClientEventHandler.java
@@ -6,6 +6,10 @@ import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSkull;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import roadhog360.simpleskinbackport.ducks.INewModelData;
@@ -21,6 +25,10 @@ public class ClientEventHandler {
     @SubscribeEvent
     public void onRenderEntity(RenderLivingEvent.Pre event) {
         checkAndSetArmsState(event.entity, event.renderer);
+        if(event.renderer instanceof RenderPlayer renderPlayer && event.entity instanceof EntityPlayer living) {
+            ItemStack itemstack = living.getEquipmentInSlot(4);
+            renderPlayer.modelBipedMain.bipedHeadwear.showModel = itemstack == null || !isHead(itemstack.getItem());
+        }
     }
 
     private void checkAndSetArmsState(Entity entity, Render render) {
@@ -28,5 +36,12 @@ public class ClientEventHandler {
             && entity instanceof INewModelData player) {
             model.setArmState(player.simpleSkinBackport$isSlim());
         }
+    }
+
+    private boolean isHead(Item item) {
+        if(item instanceof ItemSkull) {
+            return true;
+        }
+        return item.getClass().getName().equals("chylex.hee.item.ItemEndermanHead");//These don't extend ItemSkull
     }
 }
